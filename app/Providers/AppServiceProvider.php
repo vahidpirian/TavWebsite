1501f18c6d06c076c6934a99844d2f2d32f72c63
mamadjavad <mamadjavadrasti@gmail.com>
2025-02-24 23:23:08 +0330

    fix bugs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') == 'production') {
             URL::forceScheme('https');
         }
 
