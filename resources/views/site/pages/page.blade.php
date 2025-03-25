a6703db1388fda3db14086335a26132057f6f4f6
mamadjavad <mamadjavadrasti@gmail.com>
2025-03-25 23:47:27 +0330

    width 100% img header

--- resources/views/site/pages/page.blade.php
+++ resources/views/site/pages/page.blade.php
@@ -1,18 +1,18 @@
 @extends('site.layouts.master')
 
 @section('content')
-    <div class="site-breadcrumb">
-        <div class="container">
-            <h2 class="breadcrumb-title">{{$page->title}}</h2>
-            <ul class="breadcrumb-menu">
-                <li><a href="{{ route('home') }}"><i class="far fa-home"></i> صفحه اصلی</a></li>
-                <li class="active">{{$page->title}}</li>
-            </ul>
-        </div>
-        <div class="breadcrumb-shape">
-            <img src="{{asset('app-assets/img/shape-4-svg')}}" alt="">
-        </div>
-    </div>
+{{--    <div class="site-breadcrumb">--}}
+{{--        <div class="container">--}}
+{{--            <h2 class="breadcrumb-title">{{$page->title}}</h2>--}}
+{{--            <ul class="breadcrumb-menu">--}}
+{{--                <li><a href="{{ route('home') }}"><i class="far fa-home"></i> صفحه اصلی</a></li>--}}
+{{--                <li class="active">{{$page->title}}</li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--        <div class="breadcrumb-shape">--}}
+{{--            <img src="{{asset('app-assets/img/shape-4-svg')}}" alt="">--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
     @if($banner)
         <div class="container">
